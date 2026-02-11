1575248350c731015c83a1e03947d3b301864095
Nikolas <nikolaspeixoto986@gmail.com>
2026-02-10 21:49:09 -0300

    Commit da aula do dia 10-02-2026

--- api-rest/src/app.js
+++ api-rest/src/app.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const app = express()
 
+// Indicar para o express ler o body como json
+app.use(express.json())
+
+
 //Mock
 const selecoes = [
     {id:1, selecao: 'Brasil', grupo: 'C'},
@@ -8,14 +12,58 @@ const selecoes = [
     {id:1, selecao: 'Canada', grupo: 'B'},
     {id:1, selecao: 'Alemanha', grupo: 'D'},
 ]
+
+function buscarSelecoesPorId(id){
+    return selecoes.filter(selecao => selecao.id ==id)
+}
+
+function buscarSelecoesIndex(id){
+    return selecoes.findIndex(selecao => selecao.id ==id)
+}
+
+
 // Criando uma rota padrão (ou raiz)
 app.get('/', (req, res)=>{
-    res.send(selecoes)
+    res.status(200).send(selecoes)
 })
 
 app.get('/selecoes', (req, res)=>{
-    res.send('Lista de seleções da copa do mundo!')
+    res.send(selecoes)
 })
+
+app.get('/selecoes/:id', (req, res)=>{
+   // let index = req.params.id
+    //console.log(index)
+    let index = res.json(buscarSelecoesPorId(req.params.id))
+})
+
+app.delete('/selecoes/:id', (req, res)=>{
+   // let index = req.params.id
+    //console.log(index)
+    let index = res.buscarSelecoesIndex(req.params.id)
+    selecoes.splice(index,1)
+    res.send('Seleção excluida com sucesso')
+})
+
+app.put('/selecoes/:id', (req, res)=>{
+
+    let id = req.params.id
+   // let index = req.params.id
+    //console.log(index)
+    let index = res.buscarSelecoesIndex(req.params.id)
+    //req.body.grupo
+    let nomeselecao = req.body
+    selecoes.findIndex(selecao = nomeselecao ) = 
+    res.send('Seleção atualizada com sucesso')
+})//terminar o put
+
+
+app.post('/selecoes', (req, res)=>{
+    selecoes.push(req.body)
+    res.status(200).send('Seleção cadastrada com sucesso!')
+})
+
+
 
 
 
